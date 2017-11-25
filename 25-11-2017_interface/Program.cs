545dfa39797ff92d062c8d5b7f6b65445bb4addb
ушурбакиевк <@343_3>
2017-11-25 09:45:22 +0600

    Add project files.

---
+++ 25-11-2017_interface/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _25_11_2017_interface
+{
+
+    interface IAutoShop {
+
+        string CarName { set; get; }
+        int MaxSpeed { get; set; }
+        int ID { set; get; }
+
+        void ShowInfo();
+
+        void ShowInfo(int prodID);
+
+    }
+
+
+    public class CarCity : IAutoShop
+    {
+        public string CarName { set; get; }
+        public int MaxSpeed { get; set; }
+        public int ID { set; get; }
+
+        public void ShowInfo() { }
+
+        public void ShowInfo(int prodID) { }
+
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            IAutoShop shop = new CarCity();
+
+
+        }
+    }
+}
